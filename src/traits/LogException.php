6ea30ef0c8e1da6f38834a0dbcb10998d819861c
jesuszk <devjesus.br@outlook.com>
2025-03-27 09:13:35 -0300

    feat: before create log, delete olds

--- src/traits/LogException.php
+++ src/traits/LogException.php
@@ -8,6 +8,25 @@ trait LogException
     function log(string $message, int $code, string $content)
     {
         $dir = dirname(__DIR__) . "/logs/{$this->entity}/";
+        if (is_dir($dir)) {
+            $files = glob($dir . "*.log");
+            $today = new \DateTime();
+            $days = 2;
+
+            foreach ($files as $file) {
+                $filename = basename($file, ".log");
+                $fileDate = \DateTime::createFromFormat('Y-m-d', $filename);
+
+                if ($fileDate) {
+                    $diff = $today->diff($fileDate);
+                    if ($diff->days > $days) {
+                        unlink($file);
+                    }
+                }
+            }
+        }
+
+
         if (!is_dir($dir))
             mkdir($dir, 0777, true);
 
